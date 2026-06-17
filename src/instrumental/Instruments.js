96d0bc1875f9476ac768d06694842de23c04b54b
APSwinner <ague_soria@yahoo.es>
2026-06-17 17:56:01 +0200

    Change of instruments

--- src/instrumental/Instruments.js
+++ src/instrumental/Instruments.js
@@ -1,8 +1,8 @@
 import * as Tone from "tone"
 import {audioEngine} from "../core/AudioEngine.js"
-export const piano = new Tone.Sampler({
-    urls: {A4: "A4.mp3"},
-    baseUrl: "https://tonejs.github.io/audio/salamander/",
+export const piano = new Tone.PolySynth(Tone.Synth, {
+    oscillator: {type: "triangle"},
+    envelope: {attack: 0.02, decay: 0.3, sustain: 0.9, release: 0.5}
 }).connect(audioEngine.reverb);
 
 export const synth = new Tone.PolySynth(Tone.Synth,{
@@ -10,19 +10,19 @@ export const synth = new Tone.PolySynth(Tone.Synth,{
     envelope: {attack: 0.1, decay: 0.2, sustain: 0.8, release: 1}
 }).connect(audioEngine.reverb);
 
-export const bass = new Tone.MembraneSynth({
-    envelope: {attack: 0.01, decay: 0.4, sustain: 0.2, release:1}
+export const bass = new Tone.PolySynth(Tone.FMSynth, {
+    harmonicity: 2,
+    modulationIndex: 8,
+    envelope: {attack: 0.02, decay: 0.3, sustain: 0.9, release: 0.5},
+    modulationEnvelope: {attack: 0.02, decay: 0.3, sustain: 0.9, release: 0.5}
 }).connect(audioEngine.reverb);
 
 export const instrumentSamplers = { 1: piano, 2: synth, 3: bass};
 
 
 export function releaseNote(sampler, note) {
-    if (sampler instanceof Tone.PolySynth || sampler instanceof Tone.Sampler) {
-        sampler.triggerRelease(note);
-    } else {
-        sampler.triggerRelease();
-    }
+    sampler.triggerRelease(note);
+    
 }
 
 
